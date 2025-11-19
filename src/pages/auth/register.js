42b5ae90bbc545761480365ea86b699ae52bd34b
Deepak4511 <deepaksingh451181@gmail.com>
2025-11-19 09:02:44 +0530

    updated code

---
+++ src/pages/auth/register.js
@@ -0,0 +1,86 @@
+
+      import React, { useState } from "react";
+import api from "../api/api";
+
+
+const Register = () => {
+  const [formData, setFormData] = useState({
+    userName: "",
+    email: "",
+    password: "",
+  });
+
+  const [message, setMessage] = useState("");
+
+  const handleChange = (e) => {
+    setFormData({ ...formData, [e.target.name]: e.target.value });
+  };
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    try {
+      const res = await api.post("/auth/register", formData);
+      setMessage(res.data.message);
+    } catch (error) {
+      setMessage(error.response?.data?.message || "Something went wrong");
+    }
+  };
+
+  return (
+    <div className="container my-5">
+      <div className=" auth-card row">
+        <div className="">
+        <h3 className="text-center mb-4">Register</h3>
+
+        {message && (
+          <div className="alert alert-info text-center">{message}</div>
+        )}
+
+        <form onSubmit={handleSubmit}>
+          <div className="mb-3">
+            <label>Username</label>
+            <input
+              type="text"
+              name="userName"
+              className="form-control"
+              onChange={handleChange}
+              placeholder="Enter username"
+              required
+            />
+          </div>
+
+          <div className="mb-3">
+            <label>Email</label>
+            <input
+              type="email"
+              name="email"
+              className="form-control"
+              onChange={handleChange}
+              placeholder="Enter email"
+              required
+            />
+          </div>
+
+          <div className="mb-3">
+            <label>Password</label>
+            <input
+              type="password"
+              name="password"
+              className="form-control"
+              onChange={handleChange}
+              placeholder="Enter password"
+              required
+            />
+          </div>
+
+          <button className="btn btn-primary w-100">Register</button>
+        </form>
+      </div>
+    </div>
+    </div>
+  );
+};
+
+export default Register;
+
